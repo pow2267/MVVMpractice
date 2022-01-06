cb83095560ee538bcc6d293817ee699eb15e6e23
pow2267 <pow2267@gmail.com>
2022-01-06 11:23:59 +0900

    Initial Commit

---
+++ MVVMpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMpractice
+//
+//  Created by kwon on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
